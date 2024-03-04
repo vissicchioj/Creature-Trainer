85da4e25dad88453cc469bedb3f4081564c0fe0d
vissicchioj <60723403+vissicchioj@users.noreply.github.com>
2024-03-04 18:39:22 -0500

    Initial Commit
    Some of the basic functionality. Petting increases the mood. Mood not being able to increase without hunger.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+export default function App() {
+
+  const [isPetting, setIsPetting] = useState(false)
+  const [isFeeding, setIsFeeding] = useState(false)
+  const [mood, setMood] = useState(30)
+  const [hunger, setHunger] = useState(30)
+
+  function handlePetting() {
+      setIsPetting(true)
+      setTimeout(() => {
+        setIsPetting(false);
+      }, 1000)
+    if (hunger > 35) {
+      setMood(prevMood => prevMood < 100 ? prevMood + 5 : prevMood + 0)
+    }
+  }
+
+  function handleFeeding() {
+    setIsFeeding(true)
+    setTimeout(() => {
+      setIsFeeding(false);
+    }, 1000)
+    if (hunger < 100) {
+      setHunger(prevHunger => prevHunger + 10)
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Creature Simulator</h1>
+      <h1>👾</h1>
+      <button
+        onClick={handlePetting}
+        disabled={isPetting}>
+          {isPetting ? "Petting..." : "Pet Me"}
+      </button>
+      <button
+        onClick={handleFeeding}
+        disabled={isFeeding}>
+          {isFeeding ? "Feeding..." : "Feed Me"}
+      </button>
+      <p>Mood: {mood}</p>
+      <p>Hunger: {hunger}</p>
+      <p>{hunger >= 100 && isFeeding === true ? "I'm too full!" : ''}</p>
     </div>
   );
 }
-
-export default App;
